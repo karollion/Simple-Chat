093074e0cf9238a6ea30365ef201a29c24e3c507
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-10-14 21:17:45 +0200

    add functions: addMessage, sendMessage

--- client/app.js
+++ client/app.js
@@ -12,15 +12,42 @@ let userName = '';
 
 // Login
 const login = function (e) {
-	e.preventDefault()
+	e.preventDefault();
 	if (!userNameInput.value) {
-		alert('Please add user name')
+		alert('Please add user name');
 	} else {
-		userName = userNameInput.value
-		loginForm.classList.remove('show')
-		messagesSection.classList.add('show')
+		userName = userNameInput.value;
+		loginForm.classList.remove('show');
+		messagesSection.classList.add('show');
     return userName;
 	}
-}
+};
 
-loginForm.addEventListener('submit', login)
+loginForm.addEventListener('submit', login);
+
+// Send message
+const sendMessage = function (e) {
+	e.preventDefault();
+	if (!messageContentInput.value) {
+		alert('Enter message');
+	} else {
+		addMessage(userName, messageContentInput.value);
+		messageContentInput = '';
+	}
+};
+
+addMessageForm.addEventListener('submit', sendMessage);
+
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if(author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+    <h3 class="message__author">${userName === author ? 'You' : author }</h3>
+    <div class="message__content">
+      ${content}
+    </div>
+  `;
+  messagesList.appendChild(message);
+}
